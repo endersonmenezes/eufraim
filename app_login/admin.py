fa03b430306a0518966b95c721a1ce819cb4eec3
end <enderson_mc@hotmail.com>
2019-08-17 15:17:19 -0300

    -- att models

--- app_login/admin.py
+++ app_login/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import SimuladoFormacao, SimuladoComponente, SimuladoEndereco, SimuladoResponsaveisTecnicos, \
-    SimuladoEmpresaCrea, ObrasEmAndamento
+    SimuladoEmpresaCrea, ObrasEmAndamento, SimuladoModalidade
 # Register your models here.
 
 admin.site.register(SimuladoFormacao)
@@ -9,3 +9,4 @@ admin.site.register(SimuladoEndereco)
 admin.site.register(SimuladoResponsaveisTecnicos)
 admin.site.register(SimuladoEmpresaCrea)
 admin.site.register(ObrasEmAndamento)
+admin.site.register(SimuladoModalidade)
